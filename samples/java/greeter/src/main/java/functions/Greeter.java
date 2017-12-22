8a63a2aa6631391294602fd23b5631563d257dcc
Zach Brown <zbrown@pivotal.io>
2017-12-22 11:19:52 -0800

    modified greeter function to return name game

--- samples/java/greeter/src/main/java/functions/Greeter.java
+++ samples/java/greeter/src/main/java/functions/Greeter.java
@@ -5,6 +5,14 @@ import java.util.function.Function;
 public class Greeter implements Function<String, String> {
 
 	public String apply(String name) {
-		return "Hello " + name;
+		//returns the name-game on the passed in String
+		String suffix = name.substring(1);
+		String song = name + ", " + name + ", bo-b" + suffix;
+		song = song + " Banana-fana fo-f" + suffix;
+		song = song + " Fee-fi-fo-m" + suffix);
+		song = song + "... " + name.toUpperCase() + "!";
+
+		return song;
+
 	}
 }
